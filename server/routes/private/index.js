57c4c601f735e9857457b807b2bc15bb45453ca6
Abel <canet.ach@gmail.com>
2017-11-26 22:41:14 +0100

    fix bug

--- server/routes/private/index.js
+++ server/routes/private/index.js
@@ -7,24 +7,28 @@ const getCustomersPayments = require('./handlers/getCustomersPayments')
 const getArticles = require('./handlers/getArticles')
 const getPricesList = require('./handlers/getPricesList')
 const getDeliveryNotes = require('./handlers/getDeliveryNotes')
-const updateCustomer = require('./handlers/put/updateCustomer')
-const addCustomer = require('./handlers/post/addCustomer')
-const updatePricesList = require('./handlers/put/updatePricesList')
 const getListArticleType = require('./handlers/getListArticleType')
+
+const updateCustomer = require('./handlers/put/updateCustomer')
+const updatePricesList = require('./handlers/put/updatePricesList')
+
+const addCustomer = require('./handlers/post/addCustomer')
 const addArticle = require('./handlers/post/addArticle')
 
 router.use( passport.authenticate('jwt', { session: false } ) )
 
-router.get('/customers', getCustomers)
+router.get('/customers',getCustomers)
 router.get('/customers/payments', getCustomersPayments)
-router.put('/customer/:id',updateCustomer)
-router.put('/pricesList/:id',updatePricesList)
-router.get('/pricesList',getPricesList)
 router.get('/listArticlesType/:list',getListArticleType)
-router.post('/customer',addCustomer)
-router.post('/pricesList',addArticle)
 router.get('/articles', getArticles)
 router.get('/deliveryNotes', getDeliveryNotes)
+router.put('/customer/:id',updateCustomer)
+router.put('/pricesList/:id',updatePricesList)
+router.post('/customer',addCustomer)
+
+router.route('/pricesList')
+  .post(addArticle)
+  .get(getPricesList)
 
 
 module.exports = router
